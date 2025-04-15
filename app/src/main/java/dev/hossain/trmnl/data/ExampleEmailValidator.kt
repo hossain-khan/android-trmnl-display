e41510663db456145914f9c01f043acf54c4cfcb
Hossain Khan <hello@hossain.dev>
2025-04-14 21:42:06 -0400

    [REMOVED] All example template content that is no longer needed

--- app/src/main/java/dev/hossain/trmnl/data/ExampleEmailValidator.kt
+++
@@ -1,7 +0,0 @@
-package dev.hossain.trmnl.data
-
-// Example of a class that is used in traditional Dagger module
-// Does not use Anvil contributed binding
-class ExampleEmailValidator {
-    fun isValidEmail(email: String): Boolean = email.contains("@")
-}
