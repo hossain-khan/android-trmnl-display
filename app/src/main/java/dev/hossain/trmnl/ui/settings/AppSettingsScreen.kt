0994fa325829ad365601727dec708aa6f4edfa74
Hossain Khan <hello@hossain.dev>
2025-04-26 08:30:17 -0400

    [ADDED] IME padding in case it helps

--- app/src/main/java/dev/hossain/trmnl/ui/settings/AppSettingsScreen.kt
+++ app/src/main/java/dev/hossain/trmnl/ui/settings/AppSettingsScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -328,7 +329,8 @@ fun AppSettingsContent(
                     .fillMaxSize()
                     .verticalScroll(scrollState)
                     .padding(innerPadding)
-                    .padding(horizontal = 32.dp),
+                    .padding(horizontal = 32.dp)
+                    .imePadding(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
         ) {
